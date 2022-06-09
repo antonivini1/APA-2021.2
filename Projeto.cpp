b13579323a3f93c5dfca26e4cd3c64d98a65e799
Antoni Vinicius <72105624+antonivini1@users.noreply.github.com>
2022-06-08 21:01:39 -0300

    Add files via upload

--- Projeto.cpp
+++ Projeto.cpp
@@ -1,19 +1,23 @@
 #include <iostream>
 #include <fstream>
-#include <sstream>
 #include <vector>
-
+#include <algorithm>
 using namespace std;
 
+string verify(string input);
+int index(vector<int> vec, int server);
 
 int main(){
-    string line;
+    // string input;
+    // cout << "Arquivo: ";
+    // cin >> input;
+    // input = verify(input);
+
     ifstream myfile ("input.txt");
-    int n_line = 1;
     int n,m,p;
-    vector<int> b;
+    vector<int> b, tempVect;
     vector<vector<int>> t, c;
-    int aux, i, j, printit;
+    int temp, i, j;
 
     if(myfile.is_open()){
         while (myfile.good()){
@@ -22,54 +26,83 @@ int main(){
             myfile >> p;
             
             for(i=0;i<m;i++){
-                myfile >> aux;
-                b.push_back(aux);
+                myfile >> temp;
+                b.push_back(temp);
             }
 
-            vector<int> tempVect;
             for(i=0;i<m;i++){
                 for(j=0;j<n;j++){
-                    myfile >> aux;
-                    tempVect.push_back(aux);
+                    myfile >> temp;
+                    tempVect.push_back(temp);
                 }
                 t.push_back(tempVect);
                 tempVect.clear();
             }
 
-            tempVect.clear();
             for(i=0;i<m;i++){
                 for(j=0;j<n;j++){
-                    myfile >> aux;
-                    tempVect.push_back(aux);
+                    myfile >> temp;
+                    tempVect.push_back(temp);
                 }
                 c.push_back(tempVect);
                 tempVect.clear();
             }
-        cout << n << " "  << m  << " "  << p  << endl;
-        cout <<  "\n";
-        for(auto& printit : b){
-            cout <<  printit << " ";
-        }
-        cout <<  "\n\n";
-        for(auto& printit : t){
-            for(auto& elem : printit){
-                cout << elem << " ";
-            }
-            cout << "\n";
-        }
-        cout <<  "\n";
-        for(auto& printit : c){
-            for(auto& elem : printit){
-                cout << elem << " ";
-            }
-            cout << "\n";
-        }
         myfile.close();
         }
     }
     else{
-        cout << "Error";
+        cout << "Error" << endl;
     }
 
+
+    vector<vector<int>> jobs_list_server(m, vector<int>(n,0));
+    vector<int> capacidade_atual(m,0);
+    i = 0, j = 0;
+    int min_c;
+
+    while(j < n){
+        for(int s=0;s<c.size();s++){
+            tempVect.push_back(c[s][j]);
+        }
+
+        min_c = *min_element(tempVect.begin(), tempVect.end());
+        i = index(tempVect, min_c);
+
+        if(capacidade_atual[i] + t[i][j] <= b[i]){
+            capacidade_atual[i] += t[i][j];
+            jobs_list_server[i][j] = 1;
+        }
+        j++;
+        tempVect.clear();
+    }
+
+    int custo_total = 0;
+    for(j=0;j<n;j++){
+        for(i=0;i<m;i++){
+            if(jobs_list_server[i][j] == 1){
+                custo_total += c[i][j];
+                break;
+            }
+            if(i == m-1){
+                custo_total += p;
+            }
+        }
+    }
+    cout << "Custo total: " << custo_total << endl;
+
     return 0;
+}
+
+
+string verify(string input){
+    string end = ".txt";
+    
+    if((input.length() >= end.length()) && (input.compare(input.length()-end.length(), end.length(), end)))
+        return input+end;
+    else
+        return input;
+}
+int index(vector<int> vec, int server){
+    auto it = find(vec.begin(), vec.end(), server);
+    return (it - vec.begin());
 }
